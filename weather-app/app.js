ee33581995666fce81cfc6fd82be0f38140f5fa4
Mahan Zendedel <43912482+Mahanmmi@users.noreply.github.com>
2019-08-11 17:01:37 +0430

    JSON got from HTTP and parsed

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,14 +1,12 @@
-console.log("Starting");
+const request = require('request');
 
+const url = 'https://api.darksky.net/forecast/abb58408ed4cc40ac3e456f9c5c0b44c/37.8267,-122.4233';
 
-setTimeout(() => {
-    console.log("2 Second Timer");
-}, 2000);
-
-
-setTimeout(() => {
-    console.log("0 Second Timer");
-}, 0);
-
-
-console.log("Stopping");
+request({ url: url, json: true }, (error, response) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    data = JSON.parse(response.body);
+    console.log(data.currently);
+});
