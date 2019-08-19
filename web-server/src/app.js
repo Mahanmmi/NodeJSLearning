1d3ea705dc4ea44d67678f5364827758392e8242
Mahan Zendedel <43912482+Mahanmmi@users.noreply.github.com>
2019-08-19 18:41:21 +0430

    Dynamic hbs files started to get used

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,7 +5,29 @@ const publicDirectoryPath = path.join(__dirname, '..', 'public');
 const app = express();
 
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Mahan Zendedel'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about',{
+        title: 'About me',
+        name: 'Mahan Zendedel'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help',{
+        title: 'Help',
+        name: 'Mahan Zendedel'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
