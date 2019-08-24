041b651920a87bd635c5e45a5bd2479b70124e74
Mahan Zendedel <43912482+Mahanmmi@users.noreply.github.com>
2019-08-24 12:59:42 +0430

    partial templates added and 404 pages handled

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,25 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
+
 const PORT = 3000;
+
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '..', 'public');
+const viewsPath = path.join(__dirname, '..', 'templates', 'views');
+const partialsPath = path.join(__dirname, '..', 'templates', 'partials');
+
 const app = express();
 
-
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
+// General Pages
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
@@ -16,14 +28,14 @@ app.get('', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-    res.render('about',{
+    res.render('about', {
         title: 'About me',
         name: 'Mahan Zendedel'
     });
 });
 
 app.get('/help', (req, res) => {
-    res.render('help',{
+    res.render('help', {
         title: 'Help',
         name: 'Mahan Zendedel'
     });
@@ -33,6 +45,21 @@ app.get('/weather', (req, res) => {
     res.send({
         forecast: `It's 35°C`,
         location: `Tehran`
+    });
+});
+
+// 404 Pages
+app.get('/help/*', (req, res) => {
+    res.render('404',{
+        title: 'Help',
+        name: 'Mahan Zendedel'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: req.url,
+        name: 'Mahan Zendedel'
     });
 });
 
