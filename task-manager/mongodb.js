54476bd399379e6bb008e9be08c1ce017588e432
Mahan Zendedel <43912482+Mahanmmi@users.noreply.github.com>
2019-08-28 17:00:25 +0430

    create in DB

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,10 +1,16 @@
 // CRUD (create read update delete) in Mongo:
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+const {MongoClient, ObjectID} = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
+const id = new ObjectID();
+console.log(id.id.length);
+console.log(id.toHexString().length);
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -13,30 +19,55 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName);
 
-    // db.collection('users').insertOne({
-    //     name: 'Mahan',
-    //     age: 19
-    // }, (error, result) => {
-    //     if(error) {
-    //         return console.log('Unable to insert user', error);
-    //     }
+    
+    {   //insert (create)
 
-    //     console.log(result.ops);
-    // });
+        // db.collection('users').insertOne({
+        //     //_id: id, // you can give an ObjectID if you want but we usually dont
+        //     name: 'lil nana',
+        //     age: 5
+        // }, (error, result) => {
+        //     if(error) {
+        //         return console.log('Unable to insert user', error);
+        //     }
 
-    db.collection('users').insertMany([
-        {
-            name: 'Sib',
-            age: 1
-        }, {
-            name: 'Sir',
-            age: 2
-        }
-    ], (error, result) => {
-        if (error) {
-            return console.log('Unable to insert docs', error);
-        }
+        //     console.log(result.ops);
+        // });
 
-        console.log(result.ops);
-    });
+        // db.collection('users').insertMany([
+        //     {
+        //         name: 'Sib',
+        //         age: 1
+        //     }, {
+        //         name: 'Sir',
+        //         age: 2
+        //     }
+        // ], (error, result) => {
+        //     if (error) {
+        //         return console.log('Unable to insert docs', error);
+        //     }
+
+        //     console.log(result.ops);
+        // });
+
+        // db.collection('tasks').insertMany([
+        //     {
+        //         description: 'Eat breakfast',
+        //         completed: true
+        //     }, {
+        //         description: 'Eat lunch',
+        //         completed: true
+        //     }, {
+        //         description: 'go to class',
+        //         completed: false
+        //     }
+        // ], (error, result) => {
+        //     if (error) {
+        //         return console.log('Unable to insert task docs', error);
+        //     }
+
+        //     console.log(result.ops);
+        // });
+    }
+
 });
